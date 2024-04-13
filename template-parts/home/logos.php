c66319f6a5dd358fe4eabf53e73020afa87ee0ee
jenea130 <jenea130@mail.ru>
2024-04-13 19:43:11 +0300

    feat: logos

---
+++ template-parts/home/logos.php
@@ -0,0 +1,15 @@
+<?php
+$logos = get_field('logos');
+$items = $logos['items'];
+?>
+
+<div class="logos">
+  <div class="container">
+    <h4 class="logos__title">Our supporters</h4>
+    <div class="logos__wrap">
+      <?php foreach ($items as $item) : ?>
+        <div class="logos__item"><img src="<?php echo $item; ?>" alt=""></div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+</div>
