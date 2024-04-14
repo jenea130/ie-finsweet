96df7661d3bfe23ed0d0f0070ec5c7a9ffb26d8a
jenea130 <jenea130@mail.ru>
2024-04-14 16:19:31 +0300

    fix: nature adaptive

--- template-parts/home/nature.php
+++ template-parts/home/nature.php
@@ -10,6 +10,9 @@ $image = $nature['image'];
 <div class="nature">
   <div class="container">
     <div class="nature__wrap">
+      <div class="nature__img">
+        <img src="<?php echo $image; ?>" alt="">
+      </div>
       <div class="nature__block">
         <div class="label nature__label"><?php echo $label; ?></div>
         <div class="nature__content shift">
@@ -17,9 +20,6 @@ $image = $nature['image'];
           <div class="nature__text"><?php echo $text; ?></div>
           <a href="" class="btn"><?php echo $button_text; ?></a>
         </div>
-      </div>
-      <div class="nature__img">
-        <img src="<?php echo $image; ?>" alt="">
       </div>
     </div>
   </div>
