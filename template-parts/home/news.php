e166cc711efec93abc66d8ef5930a6c533b10984
jenea130 <jenea130@mail.ru>
2024-04-16 16:16:13 +0300

    feat: news

--- template-parts/home/news.php
+++ template-parts/home/news.php
@@ -4,19 +4,31 @@ $title = $news['title'];
 
 $posts = new WP_Query([
   'post_type' => 'post',
-  'posts_per_page' => 2
+  'posts_per_page' => 4
 ]);
 ?>
 
 <div class="news">
   <h2 class="news__title title"><?php echo $title; ?></h2>
   <div class="news__wrap">
-    <div class="news__item">
-      <div class="news__img">
-        <img src="" alt="">
-      </div>
-      <h4 class="news__subtitle"></h4>
-      <div class="news__text text"></div>
-    </div>
+    <?php if ($posts->have_posts()) : ?>
+      <?php while ($posts->have_posts()) : ?>
+        <?php $posts->the_post(); ?>
+        <?php
+        $title = get_the_title();
+        $excerpt = get_the_excerpt();
+        $permalink = get_the_permalink();
+        $image = get_the_post_thumbnail_url();
+        ?>
+        <a href="#" class="news__item">
+          <div class="news__img">
+            <img src="<?php echo $image; ?>" alt="">
+          </div>
+          <h4 class="news__subtitle"><?php echo $title; ?></h4>
+          <div class="news__text text"><?php echo $excerpt; ?></div>
+      </a>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
   </div>
 </div>
