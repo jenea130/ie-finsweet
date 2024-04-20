f0d2d283dfae1cacd2597a11e8e11646e3457442
jenea130 <jenea130@mail.ru>
2024-04-20 15:52:32 +0300

    feat: page what-we-do finished

--- page-what-we-do.php
+++ page-what-we-do.php
@@ -4,5 +4,11 @@
   <?php get_template_part('template-parts/what-we-do/working'); ?>
 </div>
 <?php get_template_part('template-parts/what-we-do/environment'); ?>
+<div class="container">
+  <?php get_template_part('template-parts/home/projects'); ?>
+  <?php get_template_part('template-parts/home/contribute'); ?>
+  <?php get_template_part('template-parts/home/news'); ?>
+  <?php get_template_part('template-parts/home/events'); ?>
+</div>
 
 <?php get_footer(); ?>
--- template-parts/home/projects.php
+++ template-parts/home/projects.php
@@ -1,5 +1,5 @@
 <?php
-$projects = get_field('projects');
+$projects = get_field('projects', 2);
 $label = $projects['label'];
 $title = $projects['title'];
 $button_text = $projects['button_text'];
@@ -11,28 +11,32 @@ $card_posts = new WP_Query([
   'posts_per_page' => 3
 ]);
 ?>
-<div class="projects">
-  <div class="label projects__label"><?php echo $label; ?></div>
-  <h2 class="projects__title title shift"><?php echo $title; ?></h2>
-  <div class="projects__wrap">
-    <?php if ($card_posts->have_posts()) : ?>
-      <?php while ($card_posts->have_posts()) : ?>
-        <?php $card_posts->the_post(); ?>
-        <?php
-        $image = get_the_post_thumbnail_url();
-        $title = get_the_title();
-        $excerpt = get_the_excerpt();
-        ?>
-        <div class="projects__item card">
-          <img class="card__img" src="<?php echo $image; ?>" alt="">
-          <div class="card__body">
-            <h3 class="card__title"><?php echo $title; ?></h3>
-            <div class="card__text"><?php echo $excerpt; ?></div>
-            <a href="#" class="card__btn btn btn--white"><?php echo $button_text; ?></a>
-          </div>
-        </div>
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?>
+<?php if (is_page(10)) : ?>
+  <div class="projects projects--environment">
+  <?php else : ?>
+    <div class="projects">
     <?php endif; ?>
-  </div>
-</div>
+    <div class="label projects__label"><?php echo $label; ?></div>
+    <h2 class="projects__title title shift"><?php echo $title; ?></h2>
+    <div class="projects__wrap">
+      <?php if ($card_posts->have_posts()) : ?>
+        <?php while ($card_posts->have_posts()) : ?>
+          <?php $card_posts->the_post(); ?>
+          <?php
+          $image = get_the_post_thumbnail_url();
+          $title = get_the_title();
+          $excerpt = get_the_excerpt();
+          ?>
+          <div class="projects__item card">
+            <img class="card__img" src="<?php echo $image; ?>" alt="">
+            <div class="card__body">
+              <h3 class="card__title"><?php echo $title; ?></h3>
+              <div class="card__text"><?php echo $excerpt; ?></div>
+              <a href="#" class="card__btn btn btn--white"><?php echo $button_text; ?></a>
+            </div>
+          </div>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
+    </div>
+    </div>
