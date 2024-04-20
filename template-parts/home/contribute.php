7887e2cda56fce9122fdce6811d0074bd2f5859b
jenea130 <jenea130@mail.ru>
2024-04-20 18:03:43 +0300

    feat: modal

--- template-parts/home/contribute.php
+++ template-parts/home/contribute.php
@@ -11,9 +11,9 @@ $button_text_2 = $contribute['button_text_2'];
   <div class="contribute__content">
     <h2 class="contribute__title title title--white"><?php echo $title; ?></h2>
     <footer class="contribute__footer">
-    <a href="" class="btn contribute__btn"><?php echo $button_text_1; ?></a>
-    <a href="" class="btn btn--white"><?php echo $button_text_2; ?></a>
+      <a href="" class="btn contribute__btn"><?php echo $button_text_1; ?></a>
+      <a href="" class="btn btn--white"><?php echo $button_text_2; ?></a>
     </footer>
-
   </div>
+  <?php get_template_part('template-parts/home/modal'); ?>
 </div>
