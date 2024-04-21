8f664569f3ad860701cc577f186530d1131a1107
jenea130 <jenea130@mail.ru>
2024-04-21 20:00:50 +0300

    feat: contact-form

--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 
 <?php get_template_part('template-parts/contact/talk'); ?>
+<div class="container">
+  <?php get_template_part('template-parts/contact/contact-form'); ?>
+</div>
 
 <?php get_footer(); ?>
