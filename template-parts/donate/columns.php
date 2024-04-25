f2075f40eedc167363080f912a50ac5f998130eb
jenea130 <jenea130@mail.ru>
2024-04-25 19:42:20 +0300

    feat: columns

--- template-parts/donate/columns.php
+++ template-parts/donate/columns.php
@@ -1,8 +1,9 @@
+<?php 
+$columns = get_field('columns');
+$title = $columns['title'];
+$text = $columns['text'];
+?>
 <div class="columns">
-  <div class="columns__col">
-    <h2 class="title">How we use your donation</h2>
-    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Nunc ut sem vitae risus tristique posuere.</p>
-    <p class="text">Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.</p>
-  </div>
-
+  <h2 class="title"><?php echo $title; ?></h2>
+  <?php echo $text; ?>
 </div>
