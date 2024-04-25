85bc994e5acb8009bbdb6ee08f1b3bc58ed45bdb
jenea130 <jenea130@mail.ru>
2024-04-25 19:46:28 +0300

    feat: contribute on page donation

--- page-donation.php
+++ page-donation.php
@@ -2,8 +2,8 @@
 
 <?php get_template_part('template-parts/donate/making'); ?>
 <div class="container">
-
   <?php get_template_part('template-parts/donate/protect'); ?>
   <?php get_template_part('template-parts/donate/columns'); ?>
+  <?php get_template_part('template-parts/home/contribute'); ?>
 </div>
 <?php get_footer(); ?>
