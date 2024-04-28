58d49ed823f4ed2f89c99f0a87956f5a60635857
jenea130 <jenea130@mail.ru>
2024-04-28 21:45:04 +0300

    feat: plantation adaptive

--- template-parts/single-project/plantation.php
+++ template-parts/single-project/plantation.php
@@ -5,24 +5,25 @@ $items = $plantation['items'];
 
 <div class="plantation">
   <div class="container">
-    <div class="plantation__wrap">
-      <?php foreach ($items as $item) : ?>
-        <?php
-        $icon = $item['icon'];
-        $title = $item['title'];
-        $text = $item['text'];
-        ?>
-
-        <div class="plantation__item">
-          <div class="plantation__icon">
-            <?php echo $icon; ?>
+    <div class="plantation__wrapper">
+      <div class="plantation__wrap">
+        <?php foreach ($items as $item) : ?>
+          <?php
+          $icon = $item['icon'];
+          $title = $item['title'];
+          $text = $item['text'];
+          ?>
+          <div class="plantation__item">
+            <div class="plantation__icon">
+              <?php echo $icon; ?>
+            </div>
+            <div class="plantation__body">
+              <h3 class="plantation__title"><?php echo $title; ?></h3>
+              <div class="plantation__text text"><?php echo $text; ?></div>
+            </div>
           </div>
-          <div class="plantation__body">
-            <h3 class="plantation__title"><?php echo $title; ?></h3>
-            <div class="plantation__text text"><?php echo $text; ?></div>
-          </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
 </div>
